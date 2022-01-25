89125d4f0f88e67b7221b70d5c56c5d4beefbbb3
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-01-25 14:38:49 -0600

    Fix display-load

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -6,12 +6,9 @@ const requestOptions = {
   method: 'GET',
   redirect: 'follow',
 };
-export const fetchMissions = () => (dispatch) => {
-  dispatch({ type: LOAD });
-  return fetch('https://api.spacexdata.com/v3/missions', requestOptions)
-    .then((response) => response.json())
-    .then((data) => dispatch({ type: FETCH, data }));
-};
+export const fetchMissions = () => (dispatch) => fetch('https://api.spacexdata.com/v3/missions', requestOptions)
+  .then((response) => response.json())
+  .then((data) => dispatch({ type: FETCH, data }));
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {

e92995ceaf44f610b3afa3e028df752d937a0b27
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-01-26 10:08:50 -0600

    Add basic styling to profile and header

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import './profile.css';
 
@@ -6,19 +7,29 @@ const Profile = () => {
   const reserved = useSelector((state) => state.missions).missions.filter(
     (mission) => mission.reserved,
   );
-  console.log(reserved);
   return (
     <section className="profile">
       <table className="missions">
         <thead>
-          <th>
-            <h2>Missions</h2>
-          </th>
+          <tr>
+            <th>
+              <h2>Missions</h2>
+            </th>
+          </tr>
         </thead>
         <tbody>
-          {reserved.map((mission) => (
-            <tr key={mission.mission_id}><td>{mission.mission_name}</td></tr>
-          ))}
+          {reserved.length !== 0 ? (
+            reserved.map((mission) => (
+              <tr key={mission.mission_id}>
+                <td>{mission.mission_name}</td>
+              </tr>
+            ))
+          ) : (
+            <th className="not-found">
+              See a list of all your joined missions&nbsp;
+              <Link to="/missions">here.</Link>
+            </th>
+          )}
         </tbody>
       </table>
     </section>

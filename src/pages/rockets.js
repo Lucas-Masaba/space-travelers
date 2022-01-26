deab40ccf7b4ab1d4d89732dedd396559492ef16
Lucas-Masaba <masabaluke@gmail.com>
2022-01-26 19:37:09 +0300

    add rocket badges

--- src/pages/rockets.js
+++ src/pages/rockets.js
@@ -12,14 +12,56 @@ const Rockets = (props) => {
       {rockets.map((rocket) => (
         <li className="rocket_item" key={rocket.id}>
           <div className="rocket_image_container">
-            <img className="rocket_images" alt={rocket.rocket_name} src={rocket.flickr_images} />
+            <img
+              className="rocket_images"
+              alt={rocket.rocket_name}
+              src={rocket.flickr_images}
+            />
           </div>
           <div className="rocket_paragraph_container">
             <h3>{rocket.rocket_name}</h3>
-            <p>{rocket.description}</p>
+            {rocket.reserved === true && (
+              <span className="reserve_span">
+                Reserved
+                {' '}
+              </span>
+            )}
+            <span>{rocket.description}</span>
             <p>{`Type: ${rocket.rocket_type}`}</p>
-            <button onClick={() => reserveRocketsProps(rocket.id)} className="rocket_reserve" type="button">Reserve Rocket</button>
+            <button
+              onClick={() => reserveRocketsProps(rocket.id)}
+              className={`rocket_reserve ${rocket.reserved}`}
+              type="button"
+            >
+              {rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+            </button>
           </div>
+          {/* {!rocket.reserved && (
+              <button
+                onClick={() => reserveRocketsProps(rocket.id)}
+                className="rocket_reserve"
+                type="button"
+              >
+                Reserve Rocket
+              </button>
+            )}
+            {rocket.reserved === true && (
+              <button
+                type="button"
+                onClick={() => reserveRocketsProps(rocket.id)}
+              >
+                Cancel Reservation
+              </button>
+            )}
+            {rocket.reserved === false && (
+              <button
+                onClick={() => reserveRocketsProps(rocket.id)}
+                className="rocket_reserve"
+                type="button"
+              >
+                Reserve Rocket
+              </button>
+            )} */}
         </li>
       ))}
     </ul>

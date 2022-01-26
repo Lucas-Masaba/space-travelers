6d2c1587e2818831f0d7593f672306cec2045470
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-01-26 09:37:08 -0600

    Add missions to profile

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,28 @@
-// import React from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import './profile.css';
 
-const Profile = () => <p>Under construction</p>;
+const Profile = () => {
+  const reserved = useSelector((state) => state.missions).missions.filter(
+    (mission) => mission.reserved,
+  );
+  console.log(reserved);
+  return (
+    <section className="profile">
+      <table className="missions">
+        <thead>
+          <th>
+            <h2>Missions</h2>
+          </th>
+        </thead>
+        <tbody>
+          {reserved.map((mission) => (
+            <tr key={mission.mission_id}><td>{mission.mission_name}</td></tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+};
 
 export default Profile;

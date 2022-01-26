d179af1f029e377049032e0f3e3b28eb0454e79a
Lucas-Masaba <masabaluke@gmail.com>
2022-01-26 20:21:37 +0300

    Add rockets to profile

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -7,32 +7,64 @@ const Profile = () => {
   const reserved = useSelector((state) => state.missions).missions.filter(
     (mission) => mission.reserved,
   );
+  const rockets = useSelector((state) => state.rockets.filter((rocket) => rocket.reserved));
   return (
-    <section className="profile">
-      <table className="missions">
-        <thead>
-          <tr>
-            <th>
-              <h2>Missions</h2>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {reserved.length !== 0 ? (
-            reserved.map((mission) => (
-              <tr key={mission.mission_id}>
-                <td>{mission.mission_name}</td>
+    <div className="profile_container">
+      <div>
+        <section className="profile">
+          <table className="missions">
+            <thead>
+              <tr>
+                <th>
+                  <h2>My Missions</h2>
+                </th>
               </tr>
-            ))
-          ) : (
-            <th className="not-found">
-              See a list of all your joined missions&nbsp;
-              <Link to="/missions">here.</Link>
-            </th>
-          )}
-        </tbody>
-      </table>
-    </section>
+            </thead>
+            <tbody>
+              {reserved.length !== 0 ? (
+                reserved.map((mission) => (
+                  <tr key={mission.mission_id}>
+                    <td>{mission.mission_name}</td>
+                  </tr>
+                ))
+              ) : (
+                <th className="not-found">
+                  See a list of all your joined missions&nbsp;
+                  <Link to="/missions">here.</Link>
+                </th>
+              )}
+            </tbody>
+          </table>
+        </section>
+      </div>
+      <div>
+        <section className="profile">
+          <table className="missions">
+            <thead>
+              <tr>
+                <th>
+                  <h2>My Rockets</h2>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {rockets.length !== 0 ? (
+                rockets.map((rocket) => (
+                  <tr key={rocket.id}>
+                    <td>{rocket.rocket_name}</td>
+                  </tr>
+                ))
+              ) : (
+                <th className="not-found">
+                  See a list of all your rockets&nbsp;
+                  <Link to="/">here.</Link>
+                </th>
+              )}
+            </tbody>
+          </table>
+        </section>
+      </div>
+    </div>
   );
 };
 
